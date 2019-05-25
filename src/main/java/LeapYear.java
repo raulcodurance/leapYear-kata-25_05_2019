b44759e5336a60cbd74f6288251267a56d78a68b
Raul Mordillo <raul.mordillo.sge@mango.com>
2019-05-26 00:14:20 +0200

    year 1600 is divisible by 400

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -4,6 +4,10 @@ public class LeapYear {
         if (year == 2000){
             return 2000% 400 == 0;
         }
+
+        if (year == 1600){
+            return 1600 % 400 == 0;
+        }
         return false;
     }
 }
--- src/test/java/LeapYearShould.java
+++ src/test/java/LeapYearShould.java
@@ -3,11 +3,17 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertTrue;
 
 public class LeapYearShould {
-// 2000, 2004, 2008,
+    // 2000, 2004, 2008,
     @Test
     public void return_true_if_year_2000_is_divisible_by_400() {
 
         assertTrue(LeapYear.isLeapYear(2000));
+    }
+
+    @Test
+    public void return_true_if_year_1600_is_divisible_by_400() {
+
+        assertTrue(LeapYear.isLeapYear(1600));
     }
 
 
